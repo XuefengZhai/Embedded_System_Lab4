e43a5b03b0fc6c0abd8db032813e41f99e2694d2
XuefengZhai <xzhai@cmu.edu>
2013-11-22 10:18:40 -0500

    Modify Mutex

--- kernel/lock/mutex.c
+++ kernel/lock/mutex.c
@@ -3,10 +3,10 @@
  *
  * @brief Implements mutices.
  *
- * @author Harry Q Bovik < PUT YOUR NAMES HERE
+ * @author Xuefeng Zhai xzhai@cmu.edu
  *
- * 
- * @date  
+ *
+ * @date
  */
 
 //#define DEBUG_MUTEX
@@ -17,6 +17,8 @@
 #include <bits/errno.h>
 #include <arm/psr.h>
 #include <arm/exception.h>
+
+#define DEBUG_MUTEX //temp
 #ifdef DEBUG_MUTEX
 #include <exports.h> // temp
 #endif
@@ -25,18 +27,61 @@ mutex_t gtMutex[OS_NUM_MUTEX];
 
 void mutex_init()
 {
-	
+    //Initiate everything in gtMutex
+    int i = 0;
+    for (i = 0; i < OS_NUM_MUTEX; i++)
+    {
+        gtMutex[i].bAvailable = TRUE;
+        gtMutex[i].pHolding_Tcb = NULL;
+        gtMutex[i].bLock = FALSE;
+        gtMutex[i].pSleep_queue = NULL;
+    }
+    
 }
 
 int mutex_create(void)
 {
+    int j;
+    disable_interrupts();
+    
+    for (j = 0; j < OS_NUM_MUTEX; j++)
+    {
+        if (gtMutex[j].bAvailable)
+            break;
+    }
+    
+    if ( j!= OS_NUM_MUTEX)
+    {
+        gtMutex[j].bAvailable = FALSE;
+        enable_interrupts();
+        return j;
+    }
+    else
+    {
+        printf("There are no available mutex!\n");
+        enable_interrupts();
+        return -ENOMEM;
+    }
 	
-	return 1; // fix this to return the correct value
 }
 
 int mutex_lock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+	tcb_t * cur_tcb;
+    mutex_t mutex_tmp;
+    
+    disable_interrupts();
+    
+    
+    if (mutex < 0 || mutex >= OS_NUM_MUTEX)
+    {
+        enable_interrupts();
+        printf("mutes number is invalid");
+        return -EINVAL;
+    }
+    
+    
+    
 }
 
 int mutex_unlock(int mutex  __attribute__((unused)))
